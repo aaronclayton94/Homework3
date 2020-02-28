d1c1ef63d72ae1f64293399f4cda8b911f7d1ad4
Aaron <aaronclayton94@hotmail.com>
2020-02-28 21:49:05 +0800

    added copy button and started on js

---
+++ script.js
@@ -0,0 +1,24 @@
+var enter;
+var confirmNumber;
+var startGenerator;
+var confirmNumbers;
+var confirmCharacters;
+var confirmLower;
+var confirmUpper;
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
