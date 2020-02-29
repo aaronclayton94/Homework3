ecc8b26edc52ebf0300558c8685689c565c6dd7a
Aaron <aaronclayton94@hotmail.com>
2020-02-29 08:24:02 +0800

    added password generator else if

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Homework3
+# Password Generator
+
+Homework-3 Password Generator
+
+Create a random password generator using HTML, CSS and Javascript documents
+
+Generate a random password when clicking the generate button
+
+Ask the user if they want uppercase, lowercase, numbers and symbols included
+
--- script.js
+++ script.js
@@ -1,24 +1,126 @@
+
 var enter;
-var confirmNumber;
-var startGenerator;
 var confirmNumbers;
-var confirmCharacters;
-var confirmLower;
+var confirmSymbols;
 var confirmUpper;
+var confirmLower;
 
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+number = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "a", "y", "z"];
+
+space = [];
+
+var choices;
+
+var toUpper = function (a) {
+    return a.toUpperCase();
+};
+
+upperCase = lowerCase.map(toUpper);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+    pw = generatePassword();
+    document.getElementById("password").placeholder = pw;
+});
+
+
+function generatePassword() {
+    enter = Number(prompt("Choose password length between 8 and 128"));
+    if (!enter) {
+        alert("Choose between 8 and 128! Try again!");
+    } else if (enter < 8 || enter > 128) {
+        enter = Number(prompt("Choose between 8 and 128! Try again!"));
+    } else {
+        confirmNumbers = confirm("Contain numbers?");
+        confirmSymbols = confirm("Contain special characters?");
+        confirmUpper = confirm("Contain Uppercase letters?");
+        confirmLower = confirm("Contain Lowercase letters?");
+    };
+
+    if (!confirmSymbols && !confirmNumbers && !confirmUpper && !confirmLower) {
+        choices = alert("You must select at least one!");
+
+    }
+    else if (confirmSymbols && confirmNumbers && confirmUpper && confirmLower) {
+        choices = character.concat(number, lowerCase, upperCase);
+    }
+
+    else if (confirmSymbols && confirmNumbers && confirmUpper) {
+        choices = character.concat(number, upperCase);
+    }
+    else if (confirmSymbols && confirmNumbers && confirmLower) {
+        choices = character.concat(number, lowerCase);
+    }
+    else if (confirmSymbols && confirmLower && confirmUpper) {
+        choices = character.concat(lowerCase, upperCase);
+    }
+    else if (confirmNumbers && confirmLower && confirmUpper) {
+        choices = number.concat(lowerCase, upperCase);
+    }
+
+    else if (confirmSymbols && confirmNumbers) {
+        choices = character.concat(number);
+
+    } else if (confirmSymbols && confirmLower) {
+        choices = character.concat(lowerCase);
+
+    } else if (confirmSymbols && confirmUpper) {
+        choices = character.concat(upperCase);
+    }
+    else if (confirmLower && confirmNumbers) {
+        choices = lowerCase.concat(number);
+
+    } else if (confirmLower && confirmUpper) {
+        choices = lowerCase.concat(upperCase);
+
+    } else if (confirmNumbers && confirmUpper) {
+        choices = number.concat(upperCase);
+    }
+
+    else if (confirmSymbols) {
+        choices = character;
+    }
+    else if (confirmNumbers) {
+        choices = number;
+    }
+    else if (confirmLower) {
+        choices = lowerCase;
+    }
+
+    else if (confirmUpper) {
+        choices = space.concat(upperCase);
+    };
+
+
+    var password = [];
+
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+
+    var pw = password.join("");
+    UserInput(pw);
+    return pw;
 }
 
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function UserInput(pw) {
+    document.getElementById("password").textContent = pw;
+
 }
 
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", function () {
+    copyPassword();
+});
 
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
+function copyPassword() {
+    document.getElementById("password").select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard!");
 }
